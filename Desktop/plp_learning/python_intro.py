3a20660354d243197eee3c0531ed7f4f7244df72
Angel Umi <angelummi2002@gmail.com>
2024-07-15 11:42:56 +0800

    answeerd assignment for expense tacker app

---
+++ Desktop/plp_learning/python_intro.py
@@ -0,0 +1,27 @@
+#correct indentation
+# age = 18
+# if age >= 18:
+#     print("you are an adult")
+# elif age > 12:
+#     print("you are a teenager")
+# else:
+#     print("you are a child")
+
+#list comprehension
+# squares = [x**2 for x in range (10)]
+# print(squares)
+
+#for loop
+# for i in range (5):
+#     print(f"i is i")
+
+#while loop
+# c
+
+#function
+# def add(a,b):
+#     return a + b 
+# result =  add(5,8) 
+# print (result) 
+
+#lambda function
